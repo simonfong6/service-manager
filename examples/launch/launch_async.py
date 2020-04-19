40f6884d90a9d4e81b8a370c53c0f25e66523ae1
Simon <simonfong6@gmail.com>
2020-04-19 00:15:45 -0700

    multiple procs

--- examples/launch/launch_async.py
+++ examples/launch/launch_async.py
@@ -6,6 +6,8 @@ Service launcher
 from subprocess import Popen
 from subprocess import PIPE
 from shlex import split
+from typing import List
+
 
 def run(command):
     cmd_seq = split(command)
@@ -13,6 +15,7 @@ def run(command):
     proc = Popen(cmd_seq, stdout=PIPE, stdin=PIPE, universal_newlines=True)
 
     return proc
+
 
 def ask():
     out = run('python3 ask.py')
@@ -27,21 +30,46 @@ def ask():
     print(out.poll())
 
 
+
+def kill_procs(procs: List[Popen]):
+    for proc in procs:
+        proc.kill()
+
+
+def forever(num_procs: int = 2):
+    procs = []
+
+    for _ in range(num_procs):
+        proc = run('python3 forever.py')
+        procs.append(proc)
+
+    indexes = list(range(num_procs))
+
+    index_options = ','.join(indexes)
+
+    while True:
+        proc_index = input(f"Choose proc [{index_options}]: ")
+
+        proc_index = int(proc_index)
+
+        if proc_index == -1:
+            break
+        
+        print(f"Chosen proc({proc_index})")
+        proc = procs[proc_index]
+
+        msg = input("Input for proc: ")
+
+        output, errors = proc.communicate(msg)
+
+        print(f"Output from proc({proc_index})")
+        print(output)
+
+
+
 def main(args):
 
-    # procs = []
-
-    # for _ in range(2):
-    #     proc = Popen()
-
-    # while True:
-        
-    pass
-
-    # with open(output) as f:
-    #     o = f.readlines()
-    #     print(o)
-
+    forever()
 
 
 if __name__ == '__main__':
